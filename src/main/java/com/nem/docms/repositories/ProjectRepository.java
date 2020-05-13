82380f13b86be03a7ed548ab3bb93f216d5ad928
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-13 18:42:01 +0530

    get Project object done

--- src/main/java/com/nem/docms/repositories/ProjectRepository.java
+++ src/main/java/com/nem/docms/repositories/ProjectRepository.java
@@ -1,17 +1,41 @@
 package com.nem.docms.repositories;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.bson.Document;
 
 import org.springframework.stereotype.Repository;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.nem.docms.entities.Project;
 
 @Repository
 public class ProjectRepository {
+	
+	MongoClient client;
+	
+	MongoClient getClient() {
+		if(client==null) {
+			client=new MongoClient("localhost",27017);
+		}
+		return client;
+	}
 
 	public List<Project> getAll() {
-		// TODO Auto-generated method stub
-		return null;
+		MongoClient mongoClient = getClient();
+		MongoDatabase database = mongoClient.getDatabase("doc-ms");
+		MongoCollection<Document> collection = database.getCollection("Projects");
+		
+		List<Project> response = new ArrayList<Project>();
+		
+		for (Document e : collection.find()) {
+			Project pro = new Project(e.get("_id").toString(),e.getString("name"),e.getString("description"));
+			response.add(pro);
+		}
+		return response;
 	}
 
 	public List<Project> add() {
