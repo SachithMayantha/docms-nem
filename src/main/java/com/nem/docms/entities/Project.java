4bfdda4f117132095b915384456314a14b206260
Sachith Mayantha Fernando <38182512+SachithMayantha@users.noreply.github.com>
2020-05-14 13:44:46 +0530

    Remove the Getter,Setter and Constructor in Model class

--- src/main/java/com/nem/docms/entities/Project.java
+++ src/main/java/com/nem/docms/entities/Project.java
@@ -3,45 +3,13 @@ package com.nem.docms.entities;
 import org.springframework.data.annotation.Id;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@RequiredArgsConstructor
 public @Data class Project {
 	
 	@Id
 	private @NonNull String id;
 	private @NonNull String name; 
 	private @NonNull String description;
-	/*public Project(String id, String name, String description) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.description = description;
-	}
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-*/
+	
 }
